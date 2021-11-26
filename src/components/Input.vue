5003db0ce02a0bc219ca6930caaa1a549849ed23
Mod8124 remote <denisenriquebaca@gmail.com>
2021-11-26 10:41:11 -0600

    change svg

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -2,7 +2,7 @@
   <main>
           <div class="search">
           <div class="searchIcon">
-               <i class="fas fa-search"></i>
+             <svg fill="none" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="10.5" cy="10.5" r="7.5"/><line x1="21" x2="15.8" y1="21" y2="15.8"/></svg>
                 <input type="text"  v-model="search" placeholder="Search for a country">
           </div>
     </div>
@@ -70,8 +70,6 @@ export default {
         load();
         
       
-
-        console.log(countries.value)
 
       return {region, countries, error, search, matching, continent, show, click}
      } ,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,10 @@
            <header>
         <nav>
             <a href="#">Where in the world ?</a>
-            <a @click="active = !active, dark = !dark" class="moon">   <i  :class="{'fas fa-moon':dark, 'far fa-moon':active }"></i> Dark Mode</a>
+            <a @click="active = !active, dark = !dark" class="moon">       <svg  :class="{'fas fa-moon':dark, 'far fa-moon':active }" fill="#000" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>  Dark Mode</a>
         </nav>
     </header>
-       <Input :active="active" :dark="dark"/>
+       <Input/>
   </div>
   </div>
 </template>
@@ -29,3 +29,4 @@ Input
   }
 }
 </script>
+
