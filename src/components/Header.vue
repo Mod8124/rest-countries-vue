96126cb33655407141b6881363a4c14be9672f6a
Mod8124 remote <denisenriquebaca@gmail.com>
2021-11-26 10:05:51 -0600

    try something

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="home">
-  <div :class="{active: active, dark: dark}">
+    <div :class="{active: active, dark: dark}">
            <header>
         <nav>
             <a href="#">Where in the world ?</a>
             <a @click="active = !active, dark = !dark" class="moon">   <i  :class="{'fas fa-moon':dark, 'far fa-moon':active }"></i> Dark Mode</a>
         </nav>
     </header>
-       <Input :active="active" :dark="dark"/>
-  </div>
+      <slot></slot>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Input from '../components/Input.vue'
 
 export default {
-  name: 'Home',
-  components: {
-Input
+  components:{
+     
   },
   data() {
-    return {
-      active:true,
-      dark: false,
-    }
+      return {
+          active:true,
+          dark:false
+      }
   }
 }
 </script>
+
+<style>
+
+</style>
